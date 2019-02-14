3bd2e364fdb669a7a98373b03ec8d7c32e9c0a8a
Levik Naparstek <levik010@gmail.com>
2019-02-14 22:04:38 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,19 +3,19 @@
 #   (2..n/2).none?{|i| n % i == 0}
 # end
 
-def prime?(n)
-  puts "That's not an integer." unless n.is_a? Integer
-  is_prime = true
-  for i in 2..n-1
-    if n % i == 0
-      is_prime = false
-    end
-  end
-  if is_prime
-    puts "#{n} is prime!"
-  else
-    puts "#{n} is not prime."
-  end
-end
+# def prime?(n)
+#   puts "That's not an integer." unless n.is_a? Integer
+#   is_prime = true
+#   for i in 2..n-1
+#     if n % i == 0
+#       is_prime = false
+#     end
+#   end
+#   if is_prime
+#     puts "#{n} is prime!"
+#   else
+#     puts "#{n} is not prime."
+#   end
+# end
 
-prime(n)
+# prime(n)
