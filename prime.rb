8ab2770ad035f17e2c0909dc2ae90027a677fcfe
Levik Naparstek <levik010@gmail.com>
2019-02-14 22:00:09 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,3 +2,17 @@
 # def prime?(n)
 #   (2..n/2).none?{|i| n % i == 0}
 # end
+
+def prime?(number)
+  start = 2
+  primes = (start..number).to_a
+  (start..number).each do |no|
+    (start..no).each do |num|
+      if ( no % num  == 0) && num != no
+        primes.delete(no)
+        break
+      end
+    end
+  end
+  primes
+end
